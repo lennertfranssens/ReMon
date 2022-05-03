5fabfdbd2cbe38a5fdbf35c4a6d676742d831fdb
lennertfranssens <lennert.franssens@gmail.com>
2022-05-03 19:13:18 +0200

    Update folder names

--- seccomp-bpf-benchmarks-artifact/benchmarks/scripts/run_microbenchmark.sh
+++ seccomp-bpf-benchmarks-artifact/benchmarks/scripts/run_microbenchmark.sh
@@ -4,7 +4,7 @@ set -e
 cd "$(readlink -f $(dirname ${BASH_SOURCE})/../../../)"
 
 # Clear files containing output.
-rm ./eurosys2022-artifact/benchmarks/results/microbenchmark/* || :
+rm ./seccomp-bpf-benchmarks-artifact/benchmarks/results/microbenchmark/* || :
 
 
 # Optional for when you want to enable IP-MON, has no effect when kernel is not IP-MON enabled.
@@ -22,20 +22,20 @@ echo " > running native microbenchmark..."
 for __i in {1..5}
 do
     echo " > run $__i/5"
-    ../../../eurosys2022-artifact/benchmarks/microbenchmark/getpid >> ../../../eurosys2022-artifact/benchmarks/results/microbenchmark/native.out
+    ../../../seccomp-bpf-benchmarks-artifact/benchmarks/microbenchmark/getpid >> ../../../seccomp-bpf-benchmarks-artifact/benchmarks/results/microbenchmark/native.out
 done
 
-../../../eurosys2022-artifact/benchmarks/scripts/relink_glibc.sh default
+../../../seccomp-bpf-benchmarks-artifact/benchmarks/scripts/relink_glibc.sh default
 echo " > running mvee microbenchmark with wrapped access..."
 for __i in {1..5}
 do
     echo " > run $__i/5"
-    ./mvee -N 1 -- ../../../eurosys2022-artifact/benchmarks/microbenchmark/getpid >> ../../../eurosys2022-artifact/benchmarks/results/microbenchmark/default.out
+    ./mvee -N 1 -- ../../../seccomp-bpf-benchmarks-artifact/benchmarks/microbenchmark/getpid >> ../../../seccomp-bpf-benchmarks-artifact/benchmarks/results/microbenchmark/default.out
 done
 
 # Make sure the correct libc version is used for later experiments.
-../../../eurosys2022-artifact/benchmarks/scripts/relink_glibc.sh default
+../../../seccomp-bpf-benchmarks-artifact/benchmarks/scripts/relink_glibc.sh default
 
 
 # Output result.
-../../../eurosys2022-artifact/benchmarks/scripts/process_microbenchmark.sh
+../../../seccomp-bpf-benchmarks-artifact/benchmarks/scripts/process_microbenchmark.sh
