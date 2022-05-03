d409179cb7fc83512c2d97f4a340fe448392e0d4
lennertfranssens <lennert.franssens@gmail.com>
2022-05-03 19:35:15 +0200

    Reuse original parts of the bootstrap script

--- seccomp-bpf-benchmarks-artifact/bootstrap.sh
+++ seccomp-bpf-benchmarks-artifact/bootstrap.sh
@@ -25,7 +25,27 @@ sudo apt install -y clang perl libjson-perl
 cd ../IP-MON
 ruby ./generate_headers.rb
 
+cp "$__home_dir/benchmarks/patches/IP-MON/"* "$__home_dir/../IP-MON/"
+
 ./comp.sh
+mv ./libipmon.so ./libipmon-default.so
+
+#patch -p 2 -d ./ < ../eurosys2022-artifact/benchmarks/patches/ipmon-nginx.patch
+#./comp.sh
+#mv ./libipmon.so ./libipmon-nginx.so
+#patch -R -p 2 -d ./ < ../eurosys2022-artifact/benchmarks/patches/ipmon-nginx.patch
+
+#patch -p 2 -d ./ < ../eurosys2022-artifact/benchmarks/patches/ipmon-apache.patch
+#./comp.sh
+#mv ./libipmon.so ./libipmon-apache.so
+#patch -R -p 2 -d ./ < ../eurosys2022-artifact/benchmarks/patches/ipmon-apache.patch
+
+#patch -p 2 -d ./ < ../eurosys2022-artifact/benchmarks/patches/ipmon-mplayer.patch
+#./comp.sh
+#mv ./libipmon.so ./libipmon-mplayer.so
+#patch -R -p 2 -d ./ < ../eurosys2022-artifact/benchmarks/patches/ipmon-mplayer.patch
+
+ln -fs ./libipmon-default.so ./libipmon.so
 ## ReMon Setup #########################################################################################################
 
 
@@ -69,10 +89,10 @@ make
 cd ../
 
 echo "__llvm_dir=\"$__home_dir/../deps/llvm/build-tree/\"" > "$__home_dir/benchmarks/"/config.sh
-if [[ "$BUILDALL" == 1 ]]
-then
-    "$__home_dir/benchmarks/scripts/build_all.sh"
-fi
+#if [[ "$BUILDALL" == 1 ]]
+#then
+#    "$__home_dir/benchmarks/scripts/build_all.sh"
+#fi
 
 cd "$__home_dir/"
 mkdir -p ./benchmarks/results/microbenchmark
