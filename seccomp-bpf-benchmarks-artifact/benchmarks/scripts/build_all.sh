08ce7f64e92be353f7590acc5fadad9a92d9321d
lennertfranssens <lennert.franssens@gmail.com>
2022-05-27 00:02:33 +0200

    Add nginx to seccomp bpf benchmarks artifact bootstrap

--- seccomp-bpf-benchmarks-artifact/benchmarks/scripts/build_all.sh
+++ seccomp-bpf-benchmarks-artifact/benchmarks/scripts/build_all.sh
@@ -10,24 +10,24 @@ $__home_dir/nginx_build.sh \
         --wrapped-anon
 
 
-$__home_dir/mplayer_build.sh     \
-        --default                \
-        --default-no-fast-memcpy \
-        --wrapped-full
+#$__home_dir/mplayer_build.sh     \
+#        --default                \
+#        --default-no-fast-memcpy \
+#        --wrapped-full
 
 
-$__home_dir/pulseaudio_build.sh \
-        --default               \
-        --wrapped
+#$__home_dir/pulseaudio_build.sh \
+#        --default               \
+#        --wrapped
 
 
 
-$__home_dir/fontconfig_build.sh \
-        --default               \
-        --wrapped
+#$__home_dir/fontconfig_build.sh \
+#        --default               \
+#        --wrapped
 
 
-$__home_dir/apache_build.sh \
-        --base              \
-        --default           \
-        --wrapped
+#$__home_dir/apache_build.sh \
+#        --base              \
+#        --default           \
+#        --wrapped
--- seccomp-bpf-benchmarks-artifact/bootstrap.sh
+++ seccomp-bpf-benchmarks-artifact/bootstrap.sh
@@ -80,15 +80,26 @@ fi
 ## Benchmark Setup #####################################################################################################
 cd "$__home_dir/benchmarks/"
 
+if [ ! -d "$__home_dir/benchmarks/nginx/" ]
+then
+    cd "$__home_dir/benchmarks/"
+    wget http://nginx.org/download/nginx-1.18.0.tar.gz
+    tar -xf nginx-1.18.0.tar.gz
+    mv nginx-1.18.0 nginx
+    patch -d ./nginx/ -p 2 < ./patches/nginx.patch
+    cp "$__home_dir/benchmarks/conf/nginx.conf" "$__home_dir/benchmarks/nginx/conf/"
+    cp "$__home_dir/benchmarks/input/index.html" "$__home_dir/benchmarks/nginx/html/"
+fi
+
 cd "$__home_dir/benchmarks/microbenchmark/"
 make
 cd ../
 
 echo "__llvm_dir=\"$__home_dir/../deps/llvm/build-tree/\"" > "$__home_dir/benchmarks/"/config.sh
-#if [[ "$BUILDALL" == 1 ]]
-#then
-#    "$__home_dir/benchmarks/scripts/build_all.sh"
-#fi
+if [[ "$BUILDALL" == 1 ]]
+then
+    "$__home_dir/benchmarks/scripts/build_all.sh"
+fi
 
 cd "$__home_dir/"
 mkdir -p ./benchmarks/results/microbenchmark
