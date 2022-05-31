e5677a68fd64213ca0d11a6c73b3c604453c1758
lennertfranssens <lennert.franssens@gmail.com>
2022-05-31 15:54:45 +0200

    Add old IP-MON process to script

--- seccomp-bpf-benchmarks-artifact/benchmarks/scripts/process_nginx.sh
+++ seccomp-bpf-benchmarks-artifact/benchmarks/scripts/process_nginx.sh
@@ -11,3 +11,6 @@ python ../../scripts/process_wrk.py "nginx-default-ReMon-1worker"
 
 echo " > nginx ReMon with IP-MON run, 1 worker"
 python ../../scripts/process_wrk.py "nginx-ReMon-IP-MON-1worker"
+
+echo " > nginx ReMon with old IP-MON with kernel patch, 1 worker"
+python ../../scripts/process_wrk.py "nginx-IP-MON-kernel-1worker"
